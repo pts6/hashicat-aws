978dccdf6c04af250fd27b78e5b4d42e81712a9f
Piyush Shah <PTS6@pge.com>
2021-05-14 18:48:57 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/PTS6Test/s3-bucket/aws"
   version = "2.1.0"
   # insert required variables here
-  bucket = "testingl12"
   bucket_prefix = "${var.prefix}"
 }
